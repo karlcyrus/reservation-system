cb60bb13d5a454497532b159be163bb7f9e28ff8
Karl Cyrus Celda <celda.karl.cyrus110@gmail.com>
2025-06-25 15:26:18 +0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,22 @@
 <?php
-$host = getenv("DB_HOST");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
-$dbname = getenv("DB_NAME");
+$db_url = getenv("DATABASE_URL"); // Render sets this
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+if (!$db_url) {
+    die("Environment variable DATABASE_URL not set.");
+}
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$db = parse_url($db_url);
+
+$host = $db["host"];
+$port = $db["port"];
+$user = $db["user"];
+$pass = $db["pass"];
+$dbname = ltrim($db["path"], "/");
+
+try {
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
