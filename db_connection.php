f180bb39b2bd621361324f992f978988cf1baff7
Karl Cyrus Celda <celda.karl.cyrus110@gmail.com>
2025-06-25 15:08:18 +0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,12 @@
-    <?php
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "appointment_system";
+<?php
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$dbname = getenv("DB_NAME");
 
-    $conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $pass, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    ?>
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
