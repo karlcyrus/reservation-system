5349e8be5f7847b356ba370809b70b6a2c42c4dc
Karl Cyrus Celda <celda.karl.cyrus110@gmail.com>
2025-07-02 21:48:43 +0800

    Added a new section and added a fade-in transition when the page loads

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const faders = document.querySelectorAll('.fade-slide');
+
+    const appearOptions = {
+        threshold: 0.1
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+});
